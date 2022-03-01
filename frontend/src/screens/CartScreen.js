4057d9c9bb5157782d3bb0986830bf9fe3f0350b
Gilad <giladmta@gmail.com>
2022-03-01 17:31:30 +0200

    start of section 9

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -18,8 +18,6 @@ const CartScreen = ({ history }) => {
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
 
-    console.log(cartItems)
-
     useEffect(() => {
         if(productId) {
             dispatch(addToCart(productId, qty))
